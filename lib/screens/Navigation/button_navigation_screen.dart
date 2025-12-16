864934ec2c1a854e4e740b707a0fad3f54a0171f
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-12-16 22:14:10 +0545

    screen button and member dhasboard

--- lib/screens/Navigation/button_navigation_screen.dart
+++ lib/screens/Navigation/button_navigation_screen.dart
@@ -1,42 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:nepalink/screens/button_screen/about_screen.dart';
-import 'package:nepalink/screens/button_screen/card_screen.dart';
+// import 'package:nepalink/screens/button_screen/about_screen.dart';
+// import 'package:nepalink/screens/button_screen/card_screen.dart';
 import 'package:nepalink/screens/button_screen/home_screen.dart';
 import 'package:nepalink/screens/button_screen/profile_screen.dart';
 
-
-class MemberDashboard extends StatefulWidget {
-  const MemberDashboard({super.key});
+class ButtonNavigationScreen extends StatefulWidget {
+  const ButtonNavigationScreen({super.key});
 
   @override
-  State<MemberDashboard> createState() => _MemberDashboardState();
+  State<ButtonNavigationScreen> createState() => _ButtonNavigationScreenState();
 }
 
-class _MemberDashboardState extends State<MemberDashboard> {
+class _ButtonNavigationScreenState extends State<ButtonNavigationScreen> {
   int _selectedIndex = 0;
 
   List<Widget> lstBottomScreen = [
 
     const HomeScreen(),
-    const CardScreen(),
+    // const CardScreen(),
     const ProfileScreen(),
-    const AboutScreen(),
+    // const AboutScreen(),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Member Dashboard")),
+      appBar: AppBar(title: Text("Dashboard")),
       body: lstBottomScreen[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: 'Shopping',
-          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.shopping_bag),
+          //   label: 'Shopping',
+          //),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Person'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+          // BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
         ],
         backgroundColor: Colors.amber,
         selectedItemColor: Colors.white,
