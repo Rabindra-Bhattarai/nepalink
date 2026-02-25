ad07f6a033d8e586432991e0794f38b125de2a92
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-25 15:34:31 +0545

    button navigation

--- lib/features/Navigation/button_navigation_screen.dart
+++ lib/features/Navigation/button_navigation_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:nepalink/screens/button_screen/about_screen.dart';
-// import 'package:nepalink/screens/button_screen/card_screen.dart';
 import 'package:nepalink/features/button_screen/home_screen.dart';
+import 'package:nepalink/features/button_screen/booking_page.dart';
+import 'package:nepalink/features/button_screen/task_page.dart';
+import 'package:nepalink/features/button_screen/chat_page.dart';
 import 'package:nepalink/features/dashboard/presentation/pages/profile_screen.dart';
 
 class ButtonNavigationScreen extends StatefulWidget {
@@ -14,27 +15,31 @@ class ButtonNavigationScreen extends StatefulWidget {
 class _ButtonNavigationScreenState extends State<ButtonNavigationScreen> {
   int _selectedIndex = 0;
 
-  List<Widget> lstBottomScreen = [
+  // Screens for each tab
+  final List<Widget> lstBottomScreen = [
     const HomeScreen(),
-    // const CardScreen(),
+    const BookingPage(),
+    const TaskPage(),
+    const ChatPage(),
     const ProfileScreen(),
-    // const AboutScreen(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Dashboard")),
+      appBar: AppBar(title: const Text("Dashboard")),
       body: lstBottomScreen[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          // BottomNavigationBarItem(
-          //   icon: Icon(Icons.shopping_bag),
-          //   label: 'Shopping',
-          //),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Person'),
-          // BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Booking',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.assignment), label: 'Task'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
         ],
         backgroundColor: Colors.amber,
         selectedItemColor: Colors.white,
