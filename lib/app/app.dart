415764d791e21b05b7f3326c04b44f6f5d915884
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-02-28 23:21:39 +0545

    app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:nepalink/features/dashboard/presentation/pages/caregiver_dashboard.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../features/splash/splash_screen.dart';
 import '../features/auth/presentation/pages/login_screen.dart';
 import '../features/auth/presentation/pages/register_screen.dart';
 import '../features/onboarding/onboarding_screen.dart';
-// import 'screens/dashboard/caregiver/caregiver_dashboard.dart';
-//import 'screens/Navigation/button_navigation_screen.dart';
-// import 'screens/home/home_screen.dart';
+import '../features/dashboard/booking/presentation/pages/booking_page.dart';
 
-class NepalinkApp extends StatelessWidget {
+class NepalinkApp extends ConsumerWidget {
   const NepalinkApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Nepalink',
@@ -26,9 +25,7 @@ class NepalinkApp extends StatelessWidget {
         '/onboarding': (_) => const OnboardingScreen(),
         '/login': (_) => const LoginScreen(),
         '/register': (_) => const RegisterScreen(),
-        '/caregiverDashboard': (_) => const CaregiverDashboard(),
-        //'/navigation':(_)=>const ButtonNavigationScreen()
-        // '/home': (_) => const HomeScreen(),
+        '/booking': (_) => const BookingPage(),
       },
     );
   }
