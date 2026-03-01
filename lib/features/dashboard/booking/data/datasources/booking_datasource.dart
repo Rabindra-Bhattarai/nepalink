7ff79604ea6fdc99b53be80773b0f56ecc909394
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-01 22:54:24 +0545

    booking datasource

---
+++ lib/features/dashboard/booking/data/datasources/booking_datasource.dart
@@ -0,0 +1,13 @@
+import 'package:nepalink/features/dashboard/booking/domain/entities/booking_entity.dart';
+
+abstract interface class IBookingLocalDataSource {
+  Future<List<BookingEntity>> getBookingsForNurse();
+  Future<void> cacheBookings(List<BookingEntity> bookings);
+  Future<void> updateBookingStatus(String bookingId, String status);
+}
+
+abstract interface class IBookingRemoteDataSource {
+  Future<List<BookingEntity>> getBookingsForNurse();
+  Future<BookingEntity> acceptBooking(String bookingId);
+  Future<BookingEntity> declineBooking(String bookingId);
+}
