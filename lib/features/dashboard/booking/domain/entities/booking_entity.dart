30054fc6df8c513aa8a518e554422a9ef15743a3
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-01 22:55:55 +0545

    booking entity

--- lib/features/dashboard/booking/domain/entities/booking_entity.dart
+++ lib/features/dashboard/booking/domain/entities/booking_entity.dart
@@ -1,17 +1,43 @@
-class BookingEntity {
+import 'package:equatable/equatable.dart';
+
+class BookingEntity extends Equatable {
   final String id;
   final String memberName;
-  final String memberEmail;
-  final String nurseId;
-  final DateTime date;
+  final String memberPhone;
+  final String profilePic;
+  final DateTime bookingDate;
   final String status;
 
-  BookingEntity({
+  const BookingEntity({
     required this.id,
     required this.memberName,
-    required this.memberEmail,
-    required this.nurseId,
-    required this.date,
+    required this.memberPhone,
+    required this.profilePic,
+    required this.bookingDate,
     required this.status,
   });
+
+  /// For API mapping
+  factory BookingEntity.fromJson(Map<String, dynamic> json) {
+    final member = json['memberId'] ?? {};
+    return BookingEntity(
+      id: json['_id']?.toString() ?? '',
+      memberName: member['name']?.toString() ?? '',
+      memberPhone: member['phone']?.toString() ?? '',
+      profilePic: member['imageUrl']?.toString() ?? '', // safe null handling
+      bookingDate:
+          DateTime.tryParse(json['date']?.toString() ?? '') ?? DateTime.now(),
+      status: json['status']?.toString() ?? 'pending',
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+    id,
+    memberName,
+    memberPhone,
+    profilePic,
+    bookingDate,
+    status,
+  ];
 }
