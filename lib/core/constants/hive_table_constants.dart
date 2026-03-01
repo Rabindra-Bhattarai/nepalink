141062cd1149edf6d4e18c1658e80d4f35a8fce8
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-01 22:53:37 +0545

    hive table constnats

--- lib/core/constants/hive_table_constants.dart
+++ lib/core/constants/hive_table_constants.dart
@@ -1,14 +1,18 @@
 class HiveTableConstant {
-  // Private constructor
   HiveTableConstant._();
 
   // Database name
   static const String dbName = "nepalink_db";
 
-  // Tables -> Box : TypeId
+  // User
   static const int userTypeId = 0;
   static const String userTable = "user_table";
 
+  // Session
   static const int sessionTypeId = 1;
   static const String sessionTable = "session_table";
+
+  // Booking
+  static const int bookingTypeId = 2;
+  static const String bookingTable = "booking_table";
 }
