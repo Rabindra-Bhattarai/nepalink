da6fbf68c2e5bddfc7a900c684d1f454aacacbf0
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-01 22:56:04 +0545

    booking repo

--- lib/features/dashboard/booking/domain/repositories/booking_repository.dart
+++ lib/features/dashboard/booking/domain/repositories/booking_repository.dart
@@ -1,8 +1,13 @@
-import '../../data/models/booking_model.dart';
+import 'package:nepalink/features/dashboard/booking/domain/entities/booking_entity.dart';
 
-abstract class BookingRepository {
-  Future<List<BookingModel>> getMyBookings();
-  Future<BookingModel> acceptBooking(String id);
-  Future<BookingModel> declineBooking(String id);
-  Future<BookingModel> cancelBooking(String id);
+/// Contract for booking repository
+abstract interface class BookingRepository {
+  /// Fetch all bookings for a nurse
+  Future<List<BookingEntity>> getBookingsForNurse(String nurseId);
+
+  /// Accept a booking
+  Future<BookingEntity> acceptBooking(String bookingId);
+
+  /// Decline a booking
+  Future<BookingEntity> declineBooking(String bookingId);
 }
