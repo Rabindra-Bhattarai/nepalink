190e04b836fbc0d966b40ab5136b03a5c8feb358
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2026-03-05 19:51:10 +0545

    booking page

--- lib/features/dashboard/booking/presentation/pages/booking_page.dart
+++ lib/features/dashboard/booking/presentation/pages/booking_page.dart
@@ -1,5 +1,3 @@
-// lib/features/dashboard/booking/presentation/pages/booking_page.dart
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
